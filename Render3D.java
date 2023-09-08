3715e976e1b8c9dccb2f2cfa5db0d32e09092a64
Nurlis <kimbiletovnurlis03@gmail.com>
2023-09-08 22:29:20 +0600

    Added 4 Triangle into center of screen

--- Render3D.java
+++ Render3D.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Path2D;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Render3D {
     public static void main(String[] args) {
@@ -22,7 +25,34 @@ public class Render3D {
                 g2.setColor(Color.BLACK);
                 g2.fillRect(0, 0, getWidth(), getHeight());
 
-                // rendering magic will happen here
+                List<Triangle> tris = new ArrayList<>();
+                tris.add(new Triangle(new Vertex(100, 100, 100),
+                                      new Vertex(-100, -100, 100),
+                                      new Vertex(-100, 100, -100),
+                                      Color.WHITE));
+                tris.add(new Triangle(new Vertex(100, 100, 100),
+                                      new Vertex(-100, -100, 100),
+                                      new Vertex(100, -100, -100),
+                                      Color.RED));
+                tris.add(new Triangle(new Vertex(-100, 100, -100),
+                                      new Vertex(100, -100, -100),
+                                      new Vertex(-100, 100, -100),
+                                      Color.GREEN));
+                tris.add(new Triangle(new Vertex(-100, 100, -100),
+                                      new Vertex(100, -100, -100),
+                                      new Vertex(-100, -100, 100),
+                                      Color.BLUE));
+
+                g2.translate(getWidth() / 2, getHeight() / 2);
+                g2.setColor(Color.WHITE);
+                for (Triangle t : tris) {
+                    Path2D path = new Path2D.Double();
+                    path.moveTo(t.v1.x, t.v1.y);
+                    path.lineTo(t.v2.x, t.v2.y);
+                    path.lineTo(t.v3.x, t.v3.y);
+                    path.closePath();
+                    g2.draw(path);
+                }
             }
         }; 
         pane.add(renderPanel, BorderLayout.CENTER);
